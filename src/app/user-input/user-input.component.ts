d036cf05e735a02b1294bc27c7da99b2e1d57136
Raeez Nazar <raeeznazar@gmil.com>
2025-04-20 17:15:50 +0530

    design

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredExpectedDuration = '10';
+
+
+
+  onSubmit(){
+    console.log('fsfsfsd')
+  }
+
+}
